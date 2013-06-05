7935b57888b0f47734a43cc13b00e5586a876e70
Bogdan <new.tellurian@gmail.com>
2013-06-05 10:54:07 +0200

    dynamics, working version

--- invdyn.py
+++ invdyn.py
@@ -7,83 +7,85 @@ Created on Sat Jun 01 23:13:36 2013
 
 from sympy import pi,sign,sympify,simplify
 from symoro import *
-from RX90 import *
+from cartpole import *
 
-q = var('Q0:{0}'.format(NJ))
-qp = var('QP0:{0}'.format(NJ))
-qdp = var('QDP0:{0}'.format(NJ))
-w = [zeros(3,1) for i in range(NJ)]
-wi = [zeros(3,1) for i in range(NJ)]
-wp = [zeros(3,1) for i in range(NJ)]
-v = [zeros(3,1) for i in range(NJ)]
-vp = [zeros(3,1) for i in range(NJ)]
-vsp = [zeros(3,1) for i in range(NJ)]
-phij = [zeros(6,1) for i in range(NJ)]
-lamdaj = [0 for i in range(NJ)]
-effappj = [zeros(3,1) for i in range(NJ)]
-antAj = [zeros(3,3) for i in range(NJ)]
-antPj = [zeros(3,1) for i in range(NJ)]
-U = [zeros(3,3) for i in range(NJ)]
-F = [zeros(3,1) for i in range(NJ)]
-Psi = [zeros(3,1) for i in range(NJ)]
-N = [zeros(3,1) for i in range(NJ)]
-fj = [zeros(3,1) for i in range(NJ)]
-fi = [zeros(3,1) for i in range(NJ)]
-nj = [zeros(3,1) for i in range(NJ)]
-nej = [zeros(3,1) for i in range(NJ)]#[Matrix(var('CX{0},CY{0},CZ{0}'.format(i))) for i in range(NJ)]
-fej = [zeros(3,1) for i in range(NJ)]#[Matrix(var('FX{0},FY{0},FZ{0}'.format(i))) for i in range(NJ)]
-FS = var('FS0:{0}'.format(NJ))
-IA = var('IA0:{0}'.format(NJ))
-FV = var('FV0:{0}'.format(NJ))
-MS = [zeros(3,1) for i in range(NJ)]#[Matrix(var('MX{0},MY{0},MZ{0}'.format(i))) for i in range(NJ)]
-MS[2][0] = var('M2l')
-m = var('M0:{0}'.format(NJ))
-GAM = [sympify(0) for i in range(NJ)]
-J = [Matrix(3,3,var(('XX{0},XY{0},XZ{0},'
-                            'XY{0},YY{0},YZ{0},'
-                            'XZ{0},YZ{0},M{0}ll').format(i))) for i in range(NJ)]
+w = [zeros(3,1) for i in num]
+wi = [zeros(3,1) for i in num]
+wp = [zeros(3,1) for i in num]
+v = [zeros(3,1) for i in num]
+vp = [zeros(3,1) for i in num]
+vsp = [zeros(3,1) for i in num]
+phij = [zeros(6,1) for i in num]
+lamdaj = [0 for i in num]
+effappj = [zeros(3,1) for i in num]
+antAj = [zeros(3,3) for i in num]
+antPj = [zeros(3,1) for i in num]
+U = [zeros(3,3) for i in num]
+F = [zeros(3,1) for i in num]
+Psi = [zeros(3,1) for i in num]
+N = [zeros(3,1) for i in num]
+fj = [zeros(3,1) for i in num]
+fi = [zeros(3,1) for i in num]
+nj = [zeros(3,1) for i in num]
 
 sydi = {}
 tridi = {}
-for j in range(NJ):
+for j in range(NL):
+    index = num[j]
     antTj = mat_trig_replace(transform(theta[j],r[j],alpha[j],d[j]),tridi,[j],theta,alpha,gamma)
-    antAj[j] = mat_sym_replace(get_r(antTj),sydi,'A',[j])
-    antPj[j] = mat_sym_replace(get_p(antTj),sydi,'L',[j])
-G = Matrix([0,0,-var('G')])
-w[0] = zeros(3,1)#Matrix(var('w01:4'))
-wp[0] = zeros(3,1)#Matrix(var('wp01:4'))
-v[0] = zeros(3,1)#Matrix(var('v01:4'))
-vp[0] = -G#Matrix(var('vp01:4')) + G
-for j in range(NJ):
+    antAj[j] = mat_sym_replace(get_r(antTj),sydi,'A',index)
+    antPj[j] = mat_sym_replace(get_p(antTj),sydi,'L',index)
+for j in range(NL):
+    index = num[j]
     jAant = antAj[j].T
     qpj = Matrix([0,0,qp[j]])
     qdpj = Matrix([0,0,qdp[j]])
     if j != 0:
-        wi[j] = mat_sym_replace(jAant*w[ant[j]],sydi,'WI',[j])
-        w[j] = mat_sym_replace(wi[j]+(1-sigm[j])*qpj,sydi,'W',[j])
-        wp[j] =  mat_sym_replace(jAant*wp[ant[j]]+(1-sigm[j])*(qdpj+hat(wi[j])*qpj),
-                                sydi,'WP',[j])
-        U[j] = mat_sym_replace(hat(wp[j]) + hat(w[j])*hat(w[j]),sydi,'U',[j])
-        vsp[j] = mat_sym_replace(vp[ant[j]] + U[ant[j]]*antPj[j],sydi,'VSP',[j])
-        vp[j] = mat_sym_replace(jAant*vsp[j] + sigm[j]*(qdpj+2*hat(wi[j])*qpj),
-                                    sydi,'VP',[j])
-    F[j] = mat_sym_replace(m[j]*vp[j] + U[j]*MS[j],sydi,'F',[j])
-    Psi[j] = mat_sym_replace(hat(w[j])*J[j],sydi,'PSI',[j])
-    N[j] = mat_sym_replace(J[j]*wp[j] + Psi[j]*wp[j] + hat(MS[j])*vp[j],
-                        sydi,'No',[j])
-for j in reversed(range(NJ)):
-    fj[j] = mat_sym_replace(F[j] + fej[j],sydi,'E',[j])
-    nj[j] = mat_sym_replace(N[j] + nej[j],sydi,'N',[j])
+        Uant = U[ant[j]]
+        want = w[ant[j]]
+        wpant = wp[ant[j]]
+        vpant = vp[ant[j]]
+    else:
+        Uant = zeros(3,3)
+        want = W0
+        wpant = WP0
+        vpant = VP0-G
+    wi[j] = mat_sym_replace(jAant*want,sydi,'WI',index)
+    w[j] = mat_sym_replace(wi[j]+(1-sigm[j])*qpj,sydi,'W',index)
+    wp[j] =  mat_sym_replace(jAant*wpant+(1-sigm[j])*(qdpj+hat(wi[j])*qpj),
+                            sydi,'WP',index)                   
+    U[j] = mat_sym_replace(hat(w[j])*hat(w[j])+hat(wp[j]),sydi,'U',index)
+    vsp[j] = mat_sym_replace(vpant + Uant*antPj[j],sydi,'VSP',index)
+    vp[j] = mat_sym_replace(jAant*vsp[j] + sigm[j]*(qdpj+2*hat(wi[j])*qpj),
+                                sydi,'VP',index)                         
+    F[j] = mat_sym_replace(M[j]*vp[j] + U[j]*MS[j],sydi,'F',index)
+    Psi[j] = mat_sym_replace(J[j]*w[j],sydi,'PSI',index)
+    N[j] = mat_sym_replace(J[j]*wp[j] + hat(w[j])*Psi[j],
+                        sydi,'No',index)
+for j in reversed(range(NL)):
+    index = num[j]
+    fj[j] = mat_sym_replace(F[j] + fej[j],sydi,'E',index)
+    nj[j] = mat_sym_replace(N[j] + nej[j] + hat(MS[j])*vp[j],sydi,'N',index)
     i = ant[j]
-    fi[j] = mat_sym_replace(antAj[j]*fj[j],sydi,'FDI',[j])
+    fi[j] = mat_sym_replace(antAj[j]*fj[j],sydi,'FDI',index)
     if i != -1:
         fej[i] += fi[j]
         nej[i] += antAj[j]*nj[j] + hat(antPj[j])*fi[j]
-for j in range(NJ):
+for j in range(NL):
+    index = num[j]
     if sigm[j] != 2:
         GAM[j] = sym_replace((sigm[j]*fj[j]+(1-sigm[j])*nj[j])[2]+FS[j]*sign(qp[j])+
-                                FV[j]*qp[j]+IA[j]*qdp[j],sydi,'GAM',[j])
+                                FV[j]*qp[j]+IA[j]*qdp[j],sydi,'GAM',index)
 
-#for j in range(NJ):
-#    print  'GAM{0}'.format(j), '=', simplify(unfold(GAM[j],sydi))
+for j in range(NL):
+    print  'GAM{0}'.format(num[j]), '=', unfold(GAM[j],sydi)
 
+#    DV11 = sym_replace(-w[j][0]*w[j][0],sydi,'DV11',index)
+#    DV22 = sym_replace(-w[j][1]*w[j][1],sydi,'DV22',index)
+#    DV33 = sym_replace(-w[j][2]*w[j][2],sydi,'DV33',index)
+#    DV12 = sym_replace(w[j][0]*w[j][1],sydi,'DV12',index)
+#    DV13 = sym_replace(w[j][0]*w[j][2],sydi,'DV13',index)
+#    DV23 = sym_replace(w[j][1]*w[j][2],sydi,'DV23',index)
+#    Matrix([[DV33+DV22,DV12,DV13],
+#                                  [DV12,DV11+DV33,DV23],
+#                                    [DV13,DV23,DV11+DV22]])
--- jointacc.py
+++ jointacc.py
@@ -4,150 +4,153 @@ Created on Wed May 29 20:26:47 2013
 
 @author: Bogdqn
 """
-from sympy import pi, sign
+from sympy import pi, sign, zeros
 from symoro import *
+from cartpole import *
 
-#needed variables
-D3 = var('D3')
-RL4 = var('RL4')
-#table of geometric parameters RX90
-ant = (-1,0,1,2,3,4)
-sigm = (0,0,0,0,0,0)
-alpha = (0,pi/2,0,-pi/2,pi/2,-pi/2)
-d = (0,0,D3,0,0,0)
-theta = var('th1:7')
-r = (0,0,0,RL4,0,0)
-b = (0,0,0,0,0,0)
-gamma = (0,0,0,0,0,0)
-q = var('q1:7')
-qp = var('qp1:7')
-omegajant = [zeros(3,1) for i in range(6)]
-omegaj = [zeros(3,1) for i in range(6)]
-phij = [zeros(6,1) for i in range(6)]
-lamdaj = [0 for i in range(6)]
-effappj = [zeros(3,1) for i in range(6)]
-antAj = [zeros(3,3) for i in range(6)]
-antPj = [zeros(3,1) for i in range(6)]
-jKant = [zeros(6,6) for i in range(6)]
-MS = [Matrix(var('MX{0},MY{0},MZ{0}'.format(i+1))) for i in range(6)]
-GAM = var('GAM1:7')   #Torques
-na = [Matrix(var('CX{0},CY{0},CZ{0}'.format(i+1))) for i in range(6)]
-fa = [Matrix(var('FX{0},FY{0},FZ{0}'.format(i+1))) for i in range(6)]
-matJj = [Matrix(3,3,var(('XX{0},XY{0},XZ{0},'
-                            'XY{0},YY{0},YZ{0},'
-                            'XZ{0},YZ{0},ZZ{0}').format(i+1))) for i in range(6)]
+wi = [zeros(3,1) for i in range(NL+1)]
+w = [zeros(3,1) for i in range(NL+1)]
+phij = [zeros(6,1) for i in range(NL+1)]
+lamdaj = [0 for i in range(NL+1)]
+effejppj = [zeros(3,1) for i in range(NL+1)]
+antAj = [zeros(3,3) for i in range(NL+1)]
+antPj = [zeros(3,1) for i in range(NL+1)]
+jTant = [zeros(6,6) for i in range(NL+1)]
+
 sydi = {}
 tridi = {}
-for j in range(6):
-    antTj = mat_trig_replace(transform(theta[j],r[j],alpha[j],d[j]),tridi,[j],theta)
-    antAj[j] = mat_sym_replace(get_r(antTj),sydi,'A',[j])
-    antPj[j] = mat_sym_replace(get_p(antTj),sydi,'L',[j])
+for j in range(NL):
+    index = num[j]
+    antTj = mat_trig_replace(transform(theta[j],r[j],alpha[j],d[j]),tridi,[j],theta,alpha,gamma)
+    antAj[j] = mat_sym_replace(get_r(antTj),sydi,'A',index)
+    antPj[j] = mat_sym_replace(get_p(antTj),sydi,'L',index)
     jAant = antAj[j].T
-    antPjpre = hat(antPj[j])
-    if j != 0:
-        omegajant[j] = mat_sym_replace(jAant*omegaj[ant[j]],sydi,'WI',[j])
-    omegaj[j] = mat_sym_replace(omegajant[j] + (1-sigm[j])*Matrix([0,0,qp[j]]),sydi,'W',[j])
-    jKant[j] = (Matrix([jAant.row_join(-jAant*antPjpre),
-                    zeros(3).row_join(jAant)]))
+    
+    if ant[j] != -1:
+        want = w[ant[j]]
+    else:
+        want = W0
+        
+    wi[j] = mat_sym_replace(jAant*want,sydi,'WI',index)
+    w[j] = mat_sym_replace(wi[j] + (1-sigm[j])*Matrix([0,0,qp[j]]),sydi,'W',index)
+    quantT = mat_sym_replace(-jAant*hat(antPj[j]),sydi,'JPR',index)
+    jTant[j] = (Matrix([jAant.row_join(zeros(3)),
+                    quantT.row_join(jAant)]))
     if sigm[j] == 1:
         phij[j] = Matrix([0,0,0,0,0,1])
         lamdaj[j] = GAM[j]
-        effappj[j] = Matrix([0,0,0,0,0,GAM[j]])
+        effejppj[j] = Matrix([0,0,0,0,0,GAM[j]])
     elif sigm[j] == 0:
         phij[j] = Matrix([0,0,1,0,0,0])
         lamdaj[j] = GAM[j]
-        effappj[j] = Matrix([0,0,GAM[j],0,0,0])
+        effejppj[j] = Matrix([0,0,GAM[j],0,0,0])
     elif sigm[j] == 2:
         phij[j] = Matrix([0,0,0,0,0,0])
         lamdaj[j] = GAM[j]
-        effappj[j] = Matrix([0,0,0,0,0,0])
+        effejppj[j] = Matrix([0,0,0,0,0,0])
 
-vecbetetoil = [zeros(6,1) for i in range(6)]
-matMJetoil = [zeros(6,6) for i in range(6)]
-gamaj = [zeros(6,1) for i in range(6)]
-M = var('M1:7')#???
-for j in range(6):
-    member1 = mat_sym_replace(matJj[j]*omegaj[j],sydi,'JW',[j])
-    member2 = mat_sym_replace(hat(omegaj[j])*member1,sydi,'KW',[j])
-    member3 = hat(omegaj[j])*MS[j]
-    member4 = mat_sym_replace(hat(omegaj[j])*member3,sydi,'SW',[j])
-    member5 = -na[j]-member2
-    member6 = -fa[j]-member4
+vecbetetoil = [zeros(6,1) for i in range(NL+1)]
+matMJetoil = [zeros(6,6) for i in range(NL+1)]
+gamaj = [zeros(6,1) for i in range(NL+1)]
+
+for j in range(NL):
+    index = num[j]
+    member1 = mat_sym_replace(J[j]*w[j],sydi,'JW',index)
+    member2 = mat_sym_replace(hat(w[j])*member1,sydi,'KW',index)
+    member3 = hat(w[j])*MS[j]
+    member4 = mat_sym_replace(hat(w[j])*member3,sydi,'SW',index)
+    member5 = -nej[j]-member2
+    member6 = -fej[j]-member4
     vecbetetoil[j] = Matrix([member5,member6])
-
-    quant1 = mat_sym_replace(hat(omegajant[j])*Matrix([0,0,qp[j]]),sydi,'WQ',[j])
+    if ant[j] != -1:
+        want = w[ant[j]]
+    else:
+        want = W0
+    quant1 = mat_sym_replace(hat(wi[j])*Matrix([0,0,qp[j]]),sydi,'WQ',index)
     quant2 = (1-sigm[j]) * quant1
     quant3 = 2*sigm[j] * quant1
-    quant4 = hat(omegaj[ant[j]])*antPj[j]
-    quant5 = hat(omegaj[ant[j]])*quant4
+    quant4 = hat(want)*antPj[j]
+    quant5 = hat(want)*quant4
     quant6 = antAj[j].T*quant5
-    quant7 = mat_sym_replace(quant6+quant3,sydi,'LW',[j])
+    quant7 = mat_sym_replace(quant6+quant3,sydi,'LW',index)
     gamaj[j] = Matrix([quant2,quant7])
 
-    MSjpre=hat(MS[j])
-    quant8=MSjpre.T
     #TODO ask about this matrix, the meaning
-    matMJetoil[j] = Matrix([matJj[j].row_join(quant8),MSjpre.row_join(M[j]*eye(3))])
+    matMJetoil[j] = Matrix([J[j].row_join(hat(MS[j])),hat(MS[j]).T.row_join(M[j]*eye(3))])
 
-jdej = [0 for i in range(6)]
-juj = [zeros(6,1) for i in range(6)]
-jvej = [zeros(6,1) for i in range(6)]
-grandwj = [0 for i in range(6)]
-IA = var('IA1:7')
-FS = var('FS1:7')
-FV = var('FV1:7')
-for j in reversed(range(6)):
-    jvej[j] = matMJetoil[j]*phij[j]
-    if sigm[j]==2:
-        grandwj[j]=0
+jdej = [0 for i in range(NL+1)] #1/Hj
+juj= [zeros(6,1) for i in range(NL+1)] #Jj*aj/Hj
+jvej = [zeros(6,1) for i in range(NL+1)] #Jj*aj
+grandKj = [zeros(6) for i in range(NL+1)] 
+grandwj = [0 for i in range(NL+1)]
+alphaj = [zeros(6,1) for i in range(NL+1)]
+IA = var('IA0:{0}'.format(NL+1))
+FS = var('FS0:{0}'.format(NL+1))
+FV = var('FV0:{0}'.format(NL+1))
+#phi is jaj
+for j in reversed(range(NL)):
+    index = num[j]
+    jvej[j] = matMJetoil[j]*phij[j] 
+    matMJetoil[j] = matsymm_sym_replace(matMJetoil[j],sydi,'MJE',index)
+    vecbetetoil[j] = mat_sym_replace(vecbetetoil[j],sydi,'VBE',index)
+    if sigm[j] == 2:
+        grandwj[j] = 0
     else:
-        jdej[j] = sym_replace(1/(phij[j].dot(jvej[j])+IA[j]),sydi,'JD',[j])#????
-        juj[j] = mat_sym_replace(jdej[j] * jvej[j],sydi,'JU',[j])
+        jdej[j] = sym_replace(1/(phij[j].dot(jvej[j])+IA[j]),sydi,'JD',index)
+        juj[j] = mat_sym_replace(jdej[j]*jvej[j],sydi,'JU',index)
         grandwj[j] = sym_replace(phij[j].dot(vecbetetoil[j])+lamdaj[j]-FS[j]*sign(qp[j])-FV[j]*qp[j],
-                   sydi,'GW',[j])
+                   sydi,'GW',index)
+    
 
 
+    if ant[j] != -1:
+        #    	(***************************************)
+        #     (*  calculculation of"projected matrix"*)
+        #     (***************************************)
+        #??? why is this inside the condition section?
+        grandKj[j] = mat_sym_replace(matMJetoil[j]-juj[j]*jvej[j].T,sydi,'GK',index)
+        quant1 = mat_sym_replace(grandKj[j]*gamaj[j],sydi,'NG',index) 
+        quant3 = mat_sym_replace(quant1 + grandwj[j]*juj[j],sydi,'VS',index)
+        alphaj[j] = mat_sym_replace(quant3 - vecbetetoil[j],sydi,'AP',index)
+        
+        #     (***************************************)
+        #      (*  calculation of"effective matrix"*)
+        #     (***************************************)
+        quant4 = mat_sym_replace(jTant[j].T*grandKj[j],sydi,'GX',index)
+        quant5 = matsymm_sym_replace(quant4*jTant[j],sydi,'TKT',index)
+        #??? maybe separated routine is needed for replacement to evoid recursion
+        matMJetoil[ant[j]] = matMJetoil[ant[j]]+quant5
+        vecbetetoil[ant[j]] = vecbetetoil[ant[j]]-jTant[j].T*alphaj[j]
+    
+qdp = [0 for i in range(NL+1)]
+grandVp = [zeros(6,1) for i in range(NL+1)]
+grandRj = [zeros(6,1) for i in range(NL+1)]
 
-#    if ant[j]!=0:
-#    	(***************************************)
-#     (*  calculculation of"projected matrix"*)
-#     (***************************************)
-#       expr1={{juj[[e1,1]]},{juj[[e1,2]]},{juj[[e1,3]]},
-#      {juj[[e1,4]]},{juj[[e1,5]]},{juj[[e1,6]]}};
-#
-#       expr= expr1.{jvej[j]};
-#
-#       grandNj[j]=matMJetoil[j]-expr;
-#     grandNj[j]=ecritmatrice[grandNj[j],"GK",3,e1,6,6];
-#       quant1= grandNj[j].gamaj[j];
-#
-#       quant1=ecritvecteur[quant1,"NG",3,e1,6];
-#       quant2= grandwj[j] juj[j];
-#       quant3= quant1+quant2;
-#
-#       quant3= ecritvecteur[quant3,"VS",3,e1,6];
-#       alphaj[j]=quant3 - vecbetetoil[j];
-#
-#       alphaj[j]=ecritvecteur[ alphaj[j],"AP",3,e1,6];
-#
-#
-#     (***************************************)
-#      (*  calculation of"effective matrix"*)
-#     (***************************************)
-#
-#     quant4=Transpose[jxant[j]].grandNj[j];
-#     quant4=ecritmatrice[quant4,"GX",3,e1,6,6];
-#     quant5= quant4.jxant[j];
-#     quant5=ecrituresymetrie[quant5,"TKT",3,e1,6];
-#
-#    matMJetoil[[Ant[j]]]=matMJetoil[[Ant[j]]]+quant5;
-#    matMJetoil[[Ant[j]]]=
-#    ecrituresymetrie[matMJetoil[[Ant[j]]],"MJE",3,Ant[j],6];
-#
-#
-#
-#
-#
-#   vecbetetoil[[Ant[j]]]=vecbetetoil[[Ant[j]]]-
-#   Transpose[jxant[j]].alphaj[j];
-#  vecbetetoil[[Ant[j]]]=ecritvecteur[vecbetetoil[[Ant[j]]],"VBE",3,Ant[j],6]
+for j in range(NL):
+    index = num[j]
+    if ant[j] != -1:
+        grandVpant = grandVp[ant[j]]
+    else:
+        grandVpant = Matrix([WP0,VP0-G])
+    grandRj[j] = mat_sym_replace(jTant[j]*grandVpant+gamaj[j],sydi,'VR',index)
+       
+    member1 = sym_replace(juj[j].dot(grandRj[j]),sydi,'GU',index)
+
+    if sigm[j] == 2: 
+        qdp[j] = 0
+    else:
+        qdp[j] = jdej[j]*grandwj[j]- member1
+       
+    qdp[j] = sym_replace(qdp[j],sydi,"QDP",index)
+    grandVp[j] = (grandRj[j] + qdp[j]*phij[j]) 
+    grandVp[j][:3,0] = mat_sym_replace(grandVp[j][:3,0],sydi,'WP',index)
+    grandVp[j][3:,0] = mat_sym_replace(grandVp[j][3:,0],sydi,'VP',index)
+ 
+for j in range(NL):
+    index = num[j]
+    member3 = mat_sym_replace(matMJetoil[j]*grandVp[j],sydi,'DY',index)
+    couplforce = member3 - vecbetetoil[j];
+    mat_sym_replace(couplforce[:3,0],sydi,'N',index)
+    mat_sym_replace(couplforce[3:,0],sydi,'E',index)
+            
+       
