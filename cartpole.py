6825a4830c8531fa1bb7e80fb6c47ee29f4129ac
Bogdan <new.tellurian@gmail.com>
2013-06-05 10:52:42 +0200

    robot description

--- cartpole.py
+++ cartpole.py
@@ -5,14 +5,35 @@ Created on Sun Jun 02 10:24:44 2013
 @author: Bogdqn
 """
 
-from sympy import pi,var
+from sympy import pi,var,zeros, Matrix
 #table of geometric parameters RX90
-ant = (-1,0,1)
-sigm = (2,1,0)
-alpha = (0,pi/2,pi/2)
-d = (0,0,0)
-theta = (0,pi/2,var('th2'))
-r = (0,var('r1'),0)
-b = (0,0,0)
-gamma = (0,0,0)
-NJ = len(sigm)
+ant = (-1,0)
+sigm = (1,0)
+alpha = (pi/2,pi/2)
+d = (0,0)
+theta = (pi/2,var('th2'))
+r = (var('x'),0)
+b = (0,0)
+gamma = (0,0)
+num = range(1,3)
+NJ = 2
+NL = 2
+nej = [zeros(3,1) for i in num]
+fej = [zeros(3,1) for i in num]
+FS = [0 for i in num]
+IA = [0 for i in num]
+FV = [0 for i in num]
+MS = [zeros(3,1) for i in num]
+MS[1][0] = var('m2l')
+M = [var('m{0}'.format(i)) for i in num]
+GAM = [0 for i in num]
+J = [zeros(3) for i in num]
+J[1][2,2] = var('m2ll')
+G = Matrix([0,0,-var('g')])
+W0 = zeros(3,1)
+WP0 = zeros(3,1)
+V0 = zeros(3,1)
+VP0 = zeros(3,1)
+q = var('x,th')
+qp =  var('xd,thd')
+qdp = var('xdd,thdd')
