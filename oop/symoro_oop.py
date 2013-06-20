8b9c1443e575f6f3066b57ec879db0898f8fff9b
Bogdan <new.tellurian@gmail.com>
2013-06-20 09:00:02 +0200

    OOP style sketch

---
+++ oop/symoro_oop.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 19:57:45 2013
+
+@author: Bogdqn
+"""
+from sympy import Matrix, eye, zeros, sin, cos, Integer, var, trigsimp, sympify
+
+axis_dict = {'x':0,'y':1,'z':2}
+
+class Robot:
+    def __init__(self,NL,sigm,ant,theta,r,alpha,d,gamma=[],b=[],J=[],MS=[],M=[],G=Matrix([0,0,var('G3')]),
+                 GAM=[],W0=zeros(3,1),WP0=zeros(3,1),V0=zeros(3,1),VP0=zeros(3,1),q=[],qdot=[],
+                qddot=[],nej=[],fej=[],FS=[],FV=[],IA=[]):
+        self.NL = NL
+        self.sigm = sigm
+        self.ant = ant
+        self.theta = theta
+        self.r = r
+        self.alpha = alpha
+        self.d = d
+        self.set_def_attr('gamma',gamma,[Integer(0) for i in self.num])
+        self.set_def_attr('b',b,[Integer(0) for i in self.num])
+        self.W0 = W0
+        self.WP0 = WP0
+        self.V0 = V0
+        self.VP0 = VP0
+        self.num = range(1,len(sigm)+1)
+        self.set_def_attr('q',q,[var('q{0}'.format(i)) for i in self.num])
+        self.set_def_attr('qd',qd,[var('qdot{0}'.format(i)) for i in self.num])
+        self.set_def_attr('qddot',qddot,[var('qddot{0}'.format(i)) for i in self.num])
+        
+        self.G = G
+        self.set_def_attr('nej',nej,[zeros(3,1) for i in self.num])
+        self.set_def_attr('fej',fej,[zeros(3,1) for i in self.num])
+        self.set_def_attr('FS',FS,[Integer(0) for i in self.num])
+        self.set_def_attr('FV',FV,[Integer(0) for i in self.num])
+        self.set_def_attr('IA',FS,[Integer(0) for i in self.num])
+        
+        self.set_def_attr('M',M,[var('M{0}'.format(i)) for i in self.num])
+        self.set_def_attr('MS',MS,[Matrix(var('MX{0},MY{0},MZ{0}'.format(i))) for i in self.num])
+        self.set_def_attr('J',J,[Matrix(3,3,var(('XX{0},XY{0},XZ{0},'
+                            'XY{0},YY{0},YZ{0},'
+                            'XZ{0},YZ{0},ZZ{0}').format(i))) for i in self.num])
+        
+    def set_def_attr(self,attr,value,def_value):
+       if value == []:
+           setattr(self,attr,def_value)
+       else:
+           setattr(self,attr,value)
+         
+         
+def get_r(T):
+    return T[:3,:3]
+
+def get_p(T):
+    return T[:3,3]
+
+#????????????????????????????????
+def screw_transform(T):
+    R, P = get_rp(T)
+    return screw_transf_rp(R,P)
+
+#??????????????????????????????????
+def screw_transf_rp(R,P):
+    return Matrix([R.row_join(-R*hat(P)),zeros(3).row_join(R)])
+
+def hat(v):
+    return Matrix([[0,-v[2],v[1]],
+                   [v[2],0,-v[0]],
+                   [-v[1],v[0],0]])
+
+def rot(axis = 'z', th = 0):
+    if axis == 'x':
+        return  Matrix([[1, 0, 0],
+                        [0, cos(th),-sin(th)],
+                        [0, sin(th), cos(th)]])
+    elif axis == 'y':
+        return  Matrix([[cos(th), 0, sin(th)],
+                        [0, 1, 0],
+                        [-sin(th),0, cos(th)]])
+    else:
+        return  Matrix([[cos(th),-sin(th), 0],
+                        [sin(th),cos(th), 0],
+                        [0,0, 1]])
+
+def trans_vect(axis = 'z', p = 0):
+    v = zeros(3,1)
+    v[axis_dict[axis]] = p
+    return v
+
+def trans(axis = 'z', p = 0):
+    return Matrix([eye(3).row_join(trans_vect(axis,p)),
+                       [0,0,0,1]])
+
+def rot_trans(axis='z', th=0, p=0):
+    return Matrix([rot(axis,th).row_join(trans_vect(axis,p)),
+                       [0,0,0,1]])
+
+def transform(theta, r, alpha, d, gamma=0, b=0, invert = False):
+    if not invert:
+        return rot_trans('z',gamma,b)*rot_trans('x',alpha,d)*rot_trans('z',theta,r)
+    else:
+        return rot_trans('z',-theta,-r)*rot_trans('x',-alpha,-d)*rot_trans('z',-gamma,-b)
+
+def mat_trigsimp(M):
+    for i1 in range(M.shape[0]):
+        for i2 in range(M.shape[1]):
+            M[i1,i2] = trigsimp(M[i1,i2])
+
+def trig_replace(M,sym_dict,angle,number,disp = True):
+#    print number
+    sin_sym,cos_sym = var('S{0},C{0}'.format(number))
+    sym_dict[cos_sym] = cos(angle)
+    sym_dict[sin_sym] = sin(angle)
+    if disp:
+        print cos_sym, '=', sym_dict[cos_sym]
+        print sin_sym, '=', sym_dict[sin_sym]
+    return M.subs({sym_dict[sin_sym]:sin_sym, sym_dict[cos_sym]:cos_sym})
+
+
+def mat_trig_replace(M,sym_dict,index_list,theta,alpha,gamma,disp = True):
+    number = ''
+    angle = Integer(0)
+    for j in index_list:
+        index = j + 1
+        if not sympify(theta[j]).is_constant():
+            M = trig_replace(M,sym_dict,theta[j],index,disp)
+        if not sympify(alpha[j]).is_constant():
+            M = trig_replace(M,sym_dict,alpha[j],index,disp)
+        if not sympify(gamma[j]).is_constant():
+            M = trig_replace(M,sym_dict,gamma[j],index,disp)
+            if alpha[j] == 0:
+                number = 'G{0}{1}'.format(index,number)
+                angle += gamma[j]
+        number = '{0}{1}'.format(index,number)
+        angle += theta[j]
+        if angle != theta[j] and not sympify(angle).is_constant():
+            M = trig_replace(M,sym_dict,angle,number)
+    return M
+
+def sym_replace(old_sym, sym_dict, name,index = None, index_list = [],disp = True,forced = False):
+    if old_sym.count_ops() != 0 and old_sym.count_ops() != 0 or forced:
+        if index != None:
+            name = '{0}{1}'.format(name,index)
+        else:
+            for i in index_list:
+                name = '{0}{1}'.format(name,i)
+        new_sym = var(name)
+        sym_dict[new_sym] = old_sym
+        if disp:
+            print new_sym, '=', old_sym
+        return new_sym
+    else:
+        return old_sym
+
+def mat_sym_replace(M, sym_dict, name='MATRIX',index = None, disp = True):
+    for i1 in range(M.shape[0]):
+        for i2 in range(M.shape[1]):
+            if M[i1,i2].count_ops() != 0 and (-M[i1,i2]).count_ops() != 0:
+                if M.shape[1] > 1:
+                    indeces = [i1+1,i2+1]
+                else:
+                    indeces = [i1+1]
+                if index != None:
+                    indeces.append(index)
+                M[i1,i2] = sym_replace(M[i1,i2],sym_dict, name,index_list=indeces,disp = disp)
+    return M
+    
+def matsymm_sym_replace(M, sym_dict, name='MATRIX',index = None, disp = True):
+    for i2 in range(M.shape[1]):
+        for i1 in range(i2,M.shape[0]):
+            indeces = [i1+1,i2+1]
+            if index != None:
+                indeces.append(index)
+            M[i1,i2] = sym_replace(M[i1,i2],sym_dict, name,index_list=indeces,disp = disp)
+            M[i2,i1] = M[i1,i2]
+    return M
+
+def unfold(expr,symb_dict):
+    while any([symb_dict.has_key(a) for a in expr.atoms()]):
+        expr = expr.subs(symb_dict)
+    return expr
+
+def mat_unfold(M,symb_dict):
+    for i1 in range(M.shape[0]):
+        for i2 in range(M.shape[1]):
+            M[i1,i2] = unfold(M[i1,i2],symb_dict)
