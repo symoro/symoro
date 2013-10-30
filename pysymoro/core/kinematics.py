26f98fe0d4072d74337c0c0291315561ea782427
aravind <vijaravind@gmail.com>
2013-10-30 15:06:46 +0100

    Update module import

--- pysymoro/core/kinematics.py
+++ pysymoro/core/kinematics.py
@@ -8,7 +8,8 @@ Needed modules : symoro.py, geometry.py
 ECN - ARIA1 2013
 """
 from sympy import Matrix, zeros
-from pysymoro.core.symoro import Symoro, Init, hat, ZERO
+from pysymoro.core.symoro import Symoro, Init, hat
+from pysymoro.core.symoro import FAIL, ZERO
 from pysymoro.core.geometry import dgm, Transform, compute_rot_trans
 
 TERMINAL = 0
