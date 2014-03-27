c784f1c89b7487f889589856a77e1c8b1a400464
aravind <vijaravind@gmail.com>
2014-03-27 16:06:30 +0100

    Rename file
    Rename `symoroui/gui.py` to `symoroui/layout.py` and update accordingly.

--- bin/symoro-bin.py
+++ bin/symoro-bin.py
@@ -12,14 +12,14 @@ import sys
 
 import wx
 
-from symoroui import gui
+from symoroui import layout
 from symoroui import labels as ui_labels
 
 
 def main():
     app = wx.App(redirect=False)
     style = wx.DEFAULT_FRAME_STYLE ^ wx.MAXIMIZE_BOX ^ wx.RESIZE_BORDER
-    frame = gui.MainFrame(
+    frame = layout.MainFrame(
         parent=None, 
         id=wx.ID_ANY,
         title=ui_labels.MAIN_WIN['window_title'], 
