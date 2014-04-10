447df013a2967e261d27cedc383091c2598fb7b6
aravind <vijaravind@gmail.com>
2014-04-10 15:04:21 +0200

    Add `symoroutils/symbolmgr.py` file

---
+++ pysymoro/manipulator.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+"""This module contains the Robot data structure"""
+
---
+++ symoroutils/symbolmgr.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+"""This module contains the Symbol Manager tools."""
+
+
+import os
+
+
+
