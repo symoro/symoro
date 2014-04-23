bde4fc773f5610b95de010ef359d8a797fc9e74f
aravind <vijaravind@gmail.com>
2014-04-23 23:35:03 +0200

    Add `pysymoro/tests/run_all_tests.py` file

---
+++ pysymoro/tests/run_all_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import test_screw
+import test_screw6
+
+
+def main():
+    """Main function."""
+    test_screw.run_tests()
+    test_screw6.run_tests()
+
+
+if __name__ == '__main__':
+    main()
+
+
