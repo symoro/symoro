82c735cc9707a3bde0f0eef1898a7df6bb8def6d
aravind <vijaravind@gmail.com>
2014-05-24 02:45:34 +0200

    Add Direct Dynamic Model for floating base robots

--- pysymoro/dynmodel.py
+++ pysymoro/dynmodel.py
@@ -21,7 +21,7 @@ class DynModel(object):
         Hold the various components obtained during the computation of
         dynamic models (inverse and direct).
     """
-    def __init__(self, joints):
+    def __init__(self, joints, model_type='inverse'):
         """
         Constructor period.
 
@@ -29,33 +29,63 @@ class DynModel(object):
             joints: An iterative object containing all the joint
                 numbers. This is usually the `joint_nums` attribute in
                 `Robot` class.
+            model_type: A string indicating whether the model
+                corresponds to inverse or direct dynamic model.
+                `inverse` the default option is used to indicate the
+                inverse dynamic model. Similarly, `direct` is used to
+                indicate direct dynamic model.
         """
+        # model type - inverse or dynamic
+        self.model_type = model_type
         # link velocity
         self.vels = list(None for j in joints)
         # gyroscopic acceleration
         self.gammas = list(None for j in joints)
-        # wrench - external+coriolis+centrifugal
-        self.betas = list(None for j in joints)
         # relative acceleration
         self.zetas = list(None for j in joints)
-        # composite spatial inertia matrix
-        self.composite_inertias = list(None for j in joints)
-        # composite wrench
-        self.composite_betas = list(None for j in joints)
+        # wrench - external+coriolis+centrifugal
+        self.betas = list(None for j in joints)
         # link acceleration
         self.accels = list(None for j in joints)
         # reaction wrench
         self.wrenchs = list(None for j in joints)
-        # joint torque
-        self.torques = list(None for j in joints)
+        if self.model_type is 'inverse':
+            # composite spatial inertia matrix
+            self.composite_inertias = list(None for j in joints)
+            # composite wrench
+            self.composite_betas = list(None for j in joints)
+            # joint torque
+            self.torques = list(None for j in joints)
+        elif self.model_type is 'direct':
+            # similar to composite spatial inertia matrix
+            self.star_inertias = list(None for j in joints)
+            # similar to composite wrench
+            self.star_betas = list(None for j in joints)
+            # joint torques removing the effect of friction
+            self.taus = list(None for j in joints)
+            # wrench as a function tau (torque)
+            self.alphas = list(None for j in joints)
+            # inertial element corresponding to the joint in the spatial
+            # inertia matrix
+            self.joint_inertias = list(None for j in joints)
+            # spatial inertia matrix after eliminating the element
+            # corresponding to qddot - joint acceleration
+            self.no_qddot_inertias = list(None for j in joints)
+            # joint accelerations
+            self.qddots = list(None for j in joints)
 
     def __str__(self):
         str_format = ""
         # add header
-        str_format = str_format + "DynModel:\n"
-        str_format = str_format + "---------\n"
+        str_format = str_format + "DynModel ({0}):\n".format(
+            self.model_type
+        )
+        str_format = str_format + "-------------------\n"
         # get all the attributes currently in the class
-        attrs = [attr for attr in dir(self) if not attr.startswith('_')]
+        attrs = [
+            attr for attr in dir(self) \
+            if not attr.startswith('_') or not 'model_type'
+        ]
         # add each attribute
         for attr in attrs:
             items = getattr(self, attr)
@@ -225,8 +255,11 @@ def _compute_relative_acceleration(model, robo, j):
     j_zeta_j = Screw()
     # local variables
     j_a_j = robo.geos[j].axisa
-    qddot_j = robo.qddots[j]
     j_gamma_j = model.gammas[j].val
+    if model.model_type is 'inverse':
+        qddot_j = robo.qddots[j]
+    elif model.model_type is 'direct':
+        qddot_j = model.qddots[j]
     # actual computation
     j_zeta_j.val = j_gamma_j + (qddot_j * j_a_j)
     # store computed relative acceleration in model
@@ -371,7 +404,225 @@ def _compute_joint_torque(model, robo, j):
     gamma_j = wrench_term + actuator_inertia_term + \
         viscous_friction_term + coriolis_friction_term
     # store computed torque in model
-    model.torques[j] = gamma_j
+    model.torques[j] = gamma_j[0, 0]
+    return model
+
+
+def _compute_joint_inertia(model, robo, j):
+    """
+    Compute the element in the spatial inertia matrix corresponding to
+    the joint axis along with the effect of rotor inertia.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: joint number
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    h_j = 0
+    # local variables
+    j_a_j = robo.geos[j].axisa
+    ia_j = robo.dyns[j].ia
+    j_inertia_j_s = model.star_inertias[j].val
+    # actual computation
+    h_j = (j_a_j.transpose() * j_inertia_j_s * j_a_j) + ia_j
+    # store in model
+    model.joint_inertias[j] = h_j
+    return model
+
+
+def _compute_no_qddot_inertia(model, robo, j):
+    """
+    Compute the spatial inertia by eliminating the joint acceleration
+    effect.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: joint number
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    j_k_j = Screw6()
+    # local variables
+    j_a_j = robo.geos[j].axisa
+    j_inertia_j_s = model.star_inertias[j].val
+    h_j = Matrix([model.joint_inertias[j]])
+    # actual computation
+    j_k_j.val = j_inertia_j_s - (j_inertia_j_s * j_a_j * h_jinv() * \
+        j_a_j.transpose() * j_inertia_j_s)
+    # store in model
+    model.no_qddot_inertias[j] = j_k_j
+    return model
+
+
+def _compute_tau(model, robo, j):
+    """
+    Compute the joint torque by subtracting the effect of friction.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: joint number
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    tau_j = 0
+    # local variables
+    qdot_j = robo.qdots[j]
+    gamma_j = robo.torques[j]
+    f_cj = robo.dyns[j].frc
+    f_vj = robo.dyns[j].frv
+    # actual computation
+    coriolis_friction_term = f_cj * sign(qdot_j)
+    viscous_friction_term = f_vj * qdot_j
+    tau_j = gamma_j - coriolis_friction_term - viscous_friction_term
+    # store in model
+    model.taus[j] = tau_j
+    return model
+
+
+def _compute_alpha_wrench(model, robo, j):
+    """
+    Compute the wrench as a function of tau - joint torque without
+    friction.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: joint number
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    j_alpha_j = Screw()
+    # local variables
+    j_a_j = robo.geos[j].axisa
+    j_k_j = model.no_qddot_inertias[j].val
+    j_gamma_j = model.gammas[j].val
+    j_inertia_j_s = model.star_inertias[j].val
+    j_beta_j_s = model.star_betas[j].val
+    h_j = Matrix([model.joint_inertias[j]])
+    tau_j = Matrix([model.taus[j]])
+    # actual computation
+    j_alpha_j.val = (j_k_j * j_gamma_j) + \
+        (j_inertia_j_s * j_a_j * h_j.inv() * \
+        (tau_j + j_a_j.transpose() * j_beta_j_s)) - j_beta_j_s
+    # store in model
+    model.alphas[j] = j_alpha_j
+    return model
+
+
+def _compute_star_inertia(model, robo, j, i):
+    """
+    Compute the star spatial inertia matrix for link i. This matrix is
+    similar to the composite spatial inertia matrix.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: link number
+        i: antecedent value
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    i_inertia_i_s = Screw6()
+    # local variables
+    i_inertia_i = robo.dyns[i].spatial_inertia.val
+    j_s_i = robo.geos[j].tmat.s_j_wrt_i
+    j_k_j = model.no_qddot_inertias[j].val
+    # actual computation
+    i_inertia_i_s.val = i_inertia_i + (j_s_i.transpose() * j_k_j * j_s_i)
+    # store in model
+    model.star_inertias[i] = i_inertia_i_s
+    return model
+
+
+def _compute_star_beta(model, robo, j, i):
+    """
+    Compute the star beta wrench for link i. This is similar to the
+    composite beta wrench but is a function of `tau` instead of `qddot`.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: link number
+        i: antecedent value
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    i_beta_i_s = Screw()
+    # local variables
+    j_s_i = robo.geos[j].tmat.s_j_wrt_i
+    i_beta_i = model.betas[i].val
+    j_alpha_j = model.alphas[j].val
+    # actual computation
+    i_beta_i_s.val = i_beta_i - (j_s_i.transpose() * j_alpha_j)
+    # store in model
+    model.star_betas[i] = i_beta_i_s
+    return model
+
+
+def _compute_joint_acceleration(model, robo, j, i):
+    """
+    Compute the joint acceleration for joint j.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: link number
+        i: antecedent value
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    # local variables
+    j_a_j = robo.geos[j].axisa
+    j_s_i = robo.geos[j].tmat.s_j_wrt_i
+    j_gamma_j = model.gammas[j].val
+    j_inertia_j_s = model.star_inertias[j].val
+    j_beta_j_s = model.star_betas[j].val
+    h_j = Matrix([model.joint_inertias[j]])
+    tau_j = Matrix([model.taus[j]])
+    i_vdot_i = model.accels[i].val
+    # actual computation
+    qddot_j = h_j.inv() * (-j_a_j.transpose() * j_inertia_j_s * \
+        ((j_s_i * i_vdot_i) + j_gamma_j) + tau_j + \
+        (j_a_j.transpose() * j_beta_j_s))
+    # store in model
+    model.qddots[j] = qddot_j[0, 0]
+    return model
+
+
+def _compute_reaction_wrench_alpha(model, robo, j, i):
+    """
+    Compute the reaction wrench for link j as a function of alpha wrench.
+
+    Args:
+        model: An instance of DynModel
+        robo: An instance of Robot
+        j: link number
+        i: antecedent value
+
+    Returns:
+        An instance of DynModel that contains all the new values.
+    """
+    j_f_j = Screw()
+    # local variables
+    j_s_i = robo.geos[j].tmat.s_j_wrt_i
+    j_k_j = model.no_qddot_inertias[j].val
+    j_alpha_j = model.alphas[j].val
+    i_vdot_i = model.accels[i].val
+    # actual computation
+    j_f_j.val = (j_k_j * j_s_i * i_vdot_i) + j_alpha_j
+    # store in model
+    model.wrenchs[j] = j_f_j
     return model
 
 
@@ -393,8 +644,12 @@ def _compute_base_acceleration(model, robo):
     gravity = Screw()
     # local variables
     gravity.lin = robo.gravity
-    o_inertia_o_c = model.composite_inertias[0].val
-    o_beta_o_c = model.composite_betas[0].val
+    if model.model_type is 'inverse':
+        o_inertia_o_c = model.composite_inertias[0].val
+        o_beta_o_c = model.composite_betas[0].val
+    elif model.model_type is 'direct':
+        o_inertia_o_c = model.star_inertias[0].val
+        o_beta_o_c = model.star_betas[0].val
     # actual computation
     if robo.is_floating:
         # TODO: replace sympy's matrix inversion with custom function
@@ -420,7 +675,7 @@ def inverse_dynamic_model(robo):
         The inverse dynamic model of the robot.
     """
     # some book keeping variables
-    model = DynModel(robo.joint_nums)
+    model = DynModel(robo.joint_nums, 'inverse')
     # first forward recursion
     for j in robo.joint_nums:
         if j == 0: continue
@@ -484,8 +739,57 @@ def direct_dynamic_model(robo):
         The direct dynamic model of the robot.
     """
     # some book keeping variables
-    model = DynModel(robo.joint_nums)
-    # TODO: complete direct dynamic model
+    model = DynModel(robo.joint_nums, 'direct')
+    # first forward recursion
+    for j in robo.joint_nums:
+        if j == 0: continue
+        # antecedent index
+        i = robo.geos[j].ant
+        # compute j^V_j : link velocity (6x1)
+        model = _compute_link_velocity(model, robo, j, i)
+        # compute j^gamma_j : gyroscopic acceleration (6x1)
+        model = _compute_gyroscopic_acceleration(model, robo, j, i)
+        # compute j^beta_j : external+coriolis+centrifugal wrench (6x1)
+        model = _compute_beta_wrench(model, robo, j)
+    # backward recursion
+    for j in reversed(robo.joint_nums):
+        if j == 0: continue
+        if j == robo.num_joints:
+            # initialise star_inertia, star_beta
+            model.star_inertias[j] = robo.dyns[j].spatial_inertia
+            model.star_betas[j] = model.betas[j]
+        # antecedent index
+        i = robo.geos[j].ant
+        # compute H_j : joint inertia (scalar term)
+        model = _compute_joint_inertia(model, robo, j)
+        # compute j^K_j : inertia without the effect of qddot
+        model = _compute_no_qddot_inertia(model, robo, j)
+        # compute tau_j : torque removing the effect of friction params
+        model = _compute_tau(model, robo, j)
+        # compute j^alpha_j : wrench as a function of tau
+        model = _compute_alpha_wrench(model, robo, j)
+        # compute i^I_i^* : star spatial inertia
+        model = _compute_star_inertia(model, robo, j, i)
+        # compute i^beta_i^* : star beta wrench
+        model = _compute_star_beta(model, robo, j, i)
+    # second forward recursion
+    for j in robo.joint_nums:
+        if j == 0:
+            # compute 0^\dot{V}_0 : base acceleration
+            # for fixed base robots, the value returned is just the
+            # effect of gravity
+            model = _compute_base_acceleration(model, robo)
+            continue
+        # antecedent index
+        i = robo.geos[j].ant
+        # compute qddot_j : joint acceleration
+        model = _compute_joint_acceleration(model, robo, j, i)
+        # compute j^F_j : reaction wrench as a function of alpha wrench
+        model = _compute_reaction_wrench_alpha(model, robo, j, i)
+        # compute j^zeta_j : relative acceleration
+        model = _compute_relative_acceleration(model, robo, j)
+        # compute j^V_j : link acceleration
+        model = _compute_link_acceleration(model, robo, j, i)
     return model
 
 
